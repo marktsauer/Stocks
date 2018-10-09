0b1022f7b98db9aad841cf631346c1efb4102068
Mark Sauer <marktsauer@gmail.com>
2018-10-09 08:19:15 -0400

    create database

--- iexCall.py
+++ iexCall.py
@@ -3,6 +3,7 @@ import json
 import datetime
 import time
 from datetime import timedelta
+import os
 
 #Time periods in datetime format that you can perform actions on such as today - one_day
 startTime = datetime.datetime(2000,1,1) #type- datetime.datetime format- 2000-01-01 00:00:00 #use as base time to calculate against all other time
@@ -21,7 +22,7 @@ date = shortDate(today)
 symbol = 'SPY'
 
 #calls API
-def callIEXStock(symbol, date):
+def callIEXStock(date, symbol):
     URL = "https://api.iextrading.com/1.0/stock/" + symbol + "/chart/date/" + date
     # defining a params dict for the parameters to be sent to the API
     #PARAMS = {'symbols' : 'SPY'}
@@ -32,24 +33,37 @@ def callIEXStock(symbol, date):
     data = r.json()
     return(data)
 
-j = callIEXStock(symbol, date)
-#print(j)
+j = callIEXStock(date, symbol)
 
-#get data from API - type str
-d = j[0]['date']            # 20181005
-t = j[0]['minute'] + ':00'  # 09:30:00
-p = j[0]['open']            # 289.685
-#print(d,t,p)  # 20181005 09:30:00 289.685
 
-#add hiphens to date
-newDate = datetime.datetime.strptime(d,'%Y%m%d').strftime('%Y-%m-%d')
+#Database maintainance
 
-#convert API date and time to format that can be calculated in minutes
-#add new formatted date with time
-sDate = newDate + ' ' + t
-APIdateTime = datetime.datetime.strptime(sDate,"%Y-%m-%d %H:%M:%S") # type: datetime.datetime format: 2018-10-05 09:30:00
+def maintainDB():
+    dbPath = "/Users/marksauer/Documents/GitHub/Stocks/data/"
+    ext = ".json"
+    f = open(dbPath+date+ext,"a+")
+    count = len(j)
+    i = 0
+    while i < count:
+        #get data from API - type str
+        d = j[i]['date']            # 20181005
+        t = j[i]['minute'] + ':00'  # 09:30:00
+        p = j[i]['open']            # 289.685
+        #print(d,t,p)  # 20181005 09:30:00 289.685
 
-#calculate APIdateTime in minutes
-tDelta = APIdateTime - startTime # 6852 days, 9:30:00
-print(tDelta.total_seconds())
+        #add hiphens to date
+        newDate = datetime.datetime.strptime(d,'%Y%m%d').strftime('%Y-%m-%d')
 
+        #convert API date and time to format that can be calculated in minutes
+        #add new formatted date with time
+        sDate = newDate + ' ' + t
+        APIdateTime = datetime.datetime.strptime(sDate,"%Y-%m-%d %H:%M:%S") # type: datetime.datetime format: 2018-10-05 09:30:00
+
+        #calculate APIdateTime in minutes
+        tDelta = (APIdateTime - startTime).total_seconds() # 6852 days, 9:30:00
+
+        f.write(str(APIdateTime) + ',' + str(tDelta) + ',' + str(p) + '\n')
+        i += 1
+    f.close()
+
+maintainDB()
--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 import os
-d = "/Users/marksauer/Documents/GitHub/Stocks/"
-f = "test.py"
+import json
+import datetime
+import time
+#need to make sure db path exists first
+#just create /data at root
+
+startTime = datetime.datetime(2000,1,1) #type- datetime.datetime format- 2000-01-01 00:00:00 #use as base time to calculate against all other time
+one_day = datetime.timedelta(days=1)
+one_minute = datetime.timedelta(minutes=1)
+today = datetime.datetime.today().replace(second=0).replace(microsecond=0) # 2018-10-05 10:06:00
+
+
+d = "/Users/marksauer/Documents/GitHub/Stocks/data/"
+f = "test.json"
 print(os.path.isdir(d))
 print(os.path.exists(d+f))
 print(d+f)
 
 def maintainDB():
-    print(f)
+    print(today)
+
+
+maintainDB()
